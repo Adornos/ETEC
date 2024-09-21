6ca28aa04ba6542fbec6031033b18aa8ec947545
Vex_s <146260302+Adornos@users.noreply.github.com>
2024-09-21 15:59:51 -0300

    Commit - 21092024

---
+++ TPA/C#/3bim/Testes/Whiletest/Whiletest/Program.cs
@@ -0,0 +1,33 @@
+﻿string sexo;
+int homens = 0;
+for (int i = 1; i <= 4; i++)
+{
+    Console.Write("Caso " + i + "\n Digite seu Sexo (M) (F): ");
+    sexo = Console.ReadLine().ToLower();
+    if (sexo == "f")
+    {
+        Console.Write("Digite sua altura em metros: ");
+        string altura = Console.ReadLine();
+        Console.WriteLine("Sua altura é: " + altura + "m");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    else if (sexo == "m")
+    {
+        homens++;
+        Console.WriteLine("Você é um homem");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    else
+    {
+        Console.WriteLine("Sexo idefinido");
+        i--;
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
+Console.WriteLine("Houve " + homens + " homens");
+Console.ReadKey();
